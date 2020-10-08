368ca1822d0baca1c162c192961be157e4de1fe9
Antonbrn <anton.brannstrom@cmeducations.com>
2020-10-08 11:37:51 +0200

    Updates

---
+++ precache-manifest.0bc50a121cd87aaec29790b23d88a5b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5a7f5aa4364843f4c2e8368a64266c",
+    "url": "/ISS-Tracker/index.html"
+  },
+  {
+    "revision": "7c199dd0b637c5693d86",
+    "url": "/ISS-Tracker/static/css/main.649ddea2.chunk.css"
+  },
+  {
+    "revision": "2733d901de8d88aebde3",
+    "url": "/ISS-Tracker/static/js/2.652df06e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ISS-Tracker/static/js/2.652df06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c199dd0b637c5693d86",
+    "url": "/ISS-Tracker/static/js/main.8ff8743d.chunk.js"
+  },
+  {
+    "revision": "201b539e1715b91f0923",
+    "url": "/ISS-Tracker/static/js/runtime-main.e1c8c02c.js"
+  },
+  {
+    "revision": "470b64b0aa819f67f5c2e54a6ed1bea2",
+    "url": "/ISS-Tracker/static/media/spacepic2.470b64b0.jpeg"
+  }
+]);
